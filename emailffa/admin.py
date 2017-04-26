50ba8220685de4db7fa673d79aa344942bb9525d
minerva22 <minerva.moawad@gmail.com>
2017-04-26 15:37:44 +0300

    add a bunch of Email directly when you create the Job object

--- emailffa/admin.py
+++ emailffa/admin.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Job, Email
 
+
+class EmailInline(admin.StackedInline):
+    model = Email
+    extra = 6
+
 class JobAdmin(admin.ModelAdmin):  
   list_display = ('job_text', 'pub_date')
   ordering = ('job_text',) # The negative sign indicate descendent order
   list_filter = ['pub_date']
+  inlines = [EmailInline]
+
+
 admin.site.register(Job,JobAdmin)
 admin.site.register(Email)
