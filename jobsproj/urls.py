02de0bd4768db112d269fdd769cc0d73921491e7
minerva22 <minerva.moawad@gmail.com>
2017-07-18 16:11:31 +0300

    fix issue redirect / to /emailffa

--- jobsproj/urls.py
+++ jobsproj/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
 
 
 urlpatterns = [
+    url(r'^', include('emailffa.urls')),
     url(r'^emailffa/', include('emailffa.urls')),
     url(r'^admin/', admin.site.urls),
 ]
